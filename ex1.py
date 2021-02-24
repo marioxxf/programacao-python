9bbe7aabefde7a3dc89151a029e4de00141e7d0a
Mario Junior <mariogonjunior@hotmail.com>
2021-02-24 12:35:11 -0300

    Exercício Simples em Python

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# EXERCÍCIO: Elabore um formulário que pergunte ao usuário o nome, cpf, endereço, idade, altura e telefone
+# e imprima isso em um relatório organizado.
+
+nom = input('Insira seu nome:')
+cpf = input('Insira seu CPF (Somente números): ')
+end = input('Digite seu endereço: ')
+id = input('Digite sua idade: ')
+alt = input('Insira sua altura: ')
+tel = input('Digite seu telefone: ')
+
+print("Seu nome é",nom,", seu CPF é",cpf,", seu endereço é",end,', \nsua idade é',id,', sua altura é',alt,r'e seu telefone é',tel,'.')
